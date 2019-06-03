8ead32162194233eae6dc0c78759f6fc87f53ca2
juliennejm <macapinlacjulienne10@gmail.com>
2019-06-03 22:23:10 +1200

    first comm

---
+++ SavingsTracker_V1/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace SavingsTracker_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.salary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tosave = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tospend = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rent = new System.Windows.Forms.TextBox();
+            this.laundry = new System.Windows.Forms.TextBox();
+            this.topup = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.totalExp = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.savings = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(107, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Select Month";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // salary
+            // 
+            this.salary.Location = new System.Drawing.Point(13, 91);
+            this.salary.Name = "salary";
+            this.salary.Size = new System.Drawing.Size(100, 20);
+            this.salary.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SALARY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TO SAVE(%)";
+            // 
+            // tosave
+            // 
+            this.tosave.Location = new System.Drawing.Point(13, 157);
+            this.tosave.Name = "tosave";
+            this.tosave.Size = new System.Drawing.Size(53, 20);
+            this.tosave.TabIndex = 4;
+            this.tosave.TextChanged += new System.EventHandler(this.tosave_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TO SPEND";
+            // 
+            // tospend
+            // 
+            this.tospend.Location = new System.Drawing.Point(13, 201);
+            this.tospend.Name = "tospend";
+            this.tospend.Size = new System.Drawing.Size(53, 20);
+            this.tospend.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(150, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "EXPENSES";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(153, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "RENT";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(153, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "LAUNDRY";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(153, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "PH TOP-UP";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(154, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "label8";
+            // 
+            // rent
+            // 
+            this.rent.Location = new System.Drawing.Point(236, 91);
+            this.rent.Name = "rent";
+            this.rent.Size = new System.Drawing.Size(100, 20);
+            this.rent.TabIndex = 12;
+            this.rent.TextChanged += new System.EventHandler(this.rent_TextChanged);
+            // 
+            // laundry
+            // 
+            this.laundry.Location = new System.Drawing.Point(236, 108);
+            this.laundry.Name = "laundry";
+            this.laundry.Size = new System.Drawing.Size(100, 20);
+            this.laundry.TabIndex = 13;
+            this.laundry.TextChanged += new System.EventHandler(this.laundry_TextChanged);
+            // 
+            // topup
+            // 
+            this.topup.Location = new System.Drawing.Point(236, 125);
+            this.topup.Name = "topup";
+            this.topup.Size = new System.Drawing.Size(100, 20);
+            this.topup.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(236, 142);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // totalExp
+            // 
+            this.totalExp.Enabled = false;
+            this.totalExp.Location = new System.Drawing.Point(236, 201);
+            this.totalExp.Name = "totalExp";
+            this.totalExp.Size = new System.Drawing.Size(100, 20);
+            this.totalExp.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.Location = new System.Drawing.Point(157, 207);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "TOTAL EXP";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(236, 228);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 13);
+            this.label10.TabIndex = 18;
+            // 
+            // savings
+            // 
+            this.savings.Enabled = false;
+            this.savings.Location = new System.Drawing.Point(383, 91);
+            this.savings.Name = "savings";
+            this.savings.Size = new System.Drawing.Size(100, 20);
+            this.savings.TabIndex = 19;
+            this.savings.TextChanged += new System.EventHandler(this.salary_TextChanged);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(383, 70);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "SAVINGS";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(639, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(639, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 388);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.savings);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.totalExp);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.topup);
+            this.Controls.Add(this.laundry);
+            this.Controls.Add(this.rent);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tospend);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tosave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.salary);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox salary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tosave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tospend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox rent;
+        private System.Windows.Forms.TextBox laundry;
+        private System.Windows.Forms.TextBox topup;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox totalExp;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox savings;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ SavingsTracker_V1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SavingsTracker_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int temp;
+            //tosave.Text = Convert.ToString("0%");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            //var item = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            //MessageBox.Show(item);  //message box will pop-up kung ano pinili from the list
+        }
+
+        private void rent_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(rent.Text) && !string.IsNullOrEmpty(laundry.Text))
+            {
+                totalExp.Text = (Convert.ToDecimal(laundry.Text) + Convert.ToDecimal(rent.Text)).ToString();
+            }
+
+        }
+        private void laundry_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(rent.Text) && !string.IsNullOrEmpty(laundry.Text))
+            {
+                totalExp.Text = (Convert.ToDecimal(laundry.Text) + Convert.ToDecimal(rent.Text)).ToString();
+            }
+
+        }
+
+        private void salary_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(tosave.Text) && !string.IsNullOrEmpty(salary.Text))
+            {
+                //float temp = ((Convert.ToInt32(tosave.Text)/100) * Convert.ToInt32(salary.Text));
+                //decimal temp = (Convert.ToInt32(salary.Text) * (Convert.ToDecimal(tosave.Text) / 100));
+                //savings.Text = temp.ToString();
+
+                savings.Text = (Convert.ToDecimal(salary.Text) * (Convert.ToDecimal(tosave.Text) / 100)).ToString();
+            }
+        }
+
+        private void tosave_TextChanged(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(tosave.Text) && !string.IsNullOrEmpty(salary.Text))
+            {
+                //float temp = ((Convert.ToInt32(tosave.Text)/100) * Convert.ToInt32(salary.Text));
+                //decimal temp = (Convert.ToInt32(salary.Text) * (Convert.ToDecimal(tosave.Text) / 100));
+                // savings.Text = temp.ToString();
+
+                
+                savings.Text = (Convert.ToDecimal(salary.Text) * (Convert.ToDecimal(tosave.Text) / 100)).ToString(); //total savings from salary*tosave%
+
+                textBox1.Text = savings.Text;
+                textBox2.Text = salary.Text;
+
+                decimal temp = (Convert.ToDecimal(textBox2.Text) - (Convert.ToDecimal(textBox1.Text))); //total money to be spsent
+                tospend.Text = temp.ToString();
+            }
+
+        }
+    }
+}
+    
+
